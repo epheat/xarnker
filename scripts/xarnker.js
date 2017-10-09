b88f0fe156dd27af113dc4485278e343b90b7039
Evan Heaton <epheat07+github@gmail.com>
2017-10-09 15:45:12 -0400

    cleaned up a lot of the code

--- scripts/xarnker.js
+++ scripts/xarnker.js
@@ -3,17 +3,32 @@
 // created on 10/8/17
 
 var deck = new Deck();
+deck.shuffle();
 
-var discardPile = new DiscardPile(deck.draw());
+var discardPile = new DiscardPile(deck.draw(), document.getElementById("discard"));
 
-var playerHand = new Hand(deck.draw(), deck.draw(), deck.draw());
-var computerHand = new Hand(deck.draw(), deck.draw(), deck.draw());
+// this is kind of ugly... better way to do this?
+var playerHandImgElements = [document.getElementById("playerCard0"), document.getElementById("playerCard1"), document.getElementById("playerCard2"), document.getElementById("playerCard3")];
+var computerHandImgElements = [document.getElementById("computerCard0"), document.getElementById("computerCard1"), document.getElementById("computerCard2"), document.getElementById("computerCard3")];
 
-updateHands();
-updateDiscard();
+var playerHand = new Hand([deck.draw(), deck.draw(), deck.draw()], playerHandImgElements);
+var computerHand = new Hand([deck.draw(), deck.draw(), deck.draw()], computerHandImgElements);
+
+playerHand.render();
+computerHand.render();
+discardPile.render();
+
+// game loop would go about here
 
 // just for testing purposes
 function drawDiscard() {
-  discardPile.draw();
-  updateDiscard();
+  playerHand.addCard(discardPile.draw());
+  discardPile.render();
+}
+function drawDeck() {
+  playerHand.addCard(deck.draw());
+  discardPile.render();
+}
+function playerDiscard(index) {
+  discardPile.place(playerHand.discard(index));
 }
--- scripts/xarnkerDeck.js
+++ scripts/xarnkerDeck.js
@@ -6,9 +6,12 @@
 function Deck() {
   this.cards = [];
   for (var i=1; i<=40; i++) {
-    this.cards.push(new Card(i));
+    this.cards.push(new XarnkerCard(i));
   }
-
+  this.draw = function() {
+    // note: returns 'undefined' with an empty array
+    return this.cards.pop();
+  }
   // shuffle the deck in place using a fisher-yates shuffle
   // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
   this.shuffle = function() {
@@ -17,67 +20,98 @@ function Deck() {
       // pick an element to the left of the divider (unshuffled element)
       var picked = Math.floor(Math.random() * divider);
       // and swap it with the element at the divider.
-      // this causes all elements to the right of the divider to be shuffled.
+      // this causes all elements to the right of the divider to be shuffled as we iterate.
       swap(this.cards, picked, divider);
-    }
-  }
-
-  this.shuffle();
-
-  // pop and return the top card of the deck.
-  this.draw = function() {
-    if (this.cards.length > 0) {
-      return this.cards.pop();
-    } else {
-      return null;
     }
   }
 }
 
 // Hand object constructor
-function Hand(card0, card1, card2) {
+function Hand(cards, imgElements) {
   this.cards = [];
-  this.cards.push(card0);
-  this.cards.push(card1);
-  this.cards.push(card2);
-
+  for (var i=0; i<cards.length; i++) {
+    this.cards.push(cards[i]);
+  }
+  this.imgElements = [];
+  for (var i=0; i<imgElements.length; i++) {
+    this.imgElements.push(imgElements[i]);
+  }
+  // evaluate how many points this hand is worth
   this.eval = function() {
-    // evaluate how many points this hand is worth
+
+  }
+  // discard a given card from hand
+  this.discard = function(i) {
+    var discarded = this.cards[i];
+    this.cards[i] = undefined;
+    this.render();
+    return discarded;
+  }
+  // add a card to the first missing spot in the hand
+  this.addCard = function(card) {
+    for (var i=0; i<this.imgElements.length; i++) {
+      if (this.cards[i] == undefined) {
+        this.cards[i] = card;
+        this.render();
+        return;
+      }
+    }
+    return undefined;
+  }
+  // render the cards on-screen
+  this.render = function() {
+    for (var i=0; i<this.imgElements.length; i++) {
+      if (this.cards[i] != undefined) {
+        imgElements[i].setAttribute("src", "assets/deck/" + this.cards[i].img);
+        imgElements[i].style.opacity = 1;
+      } else {
+        imgElements[i].style.opacity = 0;
+      }
+    }
   }
 }
 
 // Discard pile object constructor
-function DiscardPile(topCard) {
+function DiscardPile(topCard, imgElement) {
   this.cards = [topCard];
-
-  // pop and return the top card of the discard pile.
+  this.imgElement = imgElement;
+  // pop and return the top card from a pile.
   this.draw = function() {
-    if (this.cards.length > 0) {
-      return this.cards.pop();
-    } else {
-      return null;
-    }
+    // note: returns 'undefined' with an empty array
+    return this.cards.pop();
   }
-  // place a new card on top of the discard pile
+  // place a new card on top of a pile.
   this.place = function(card) {
     this.cards.push(card);
+    this.render();
   }
   // look at the top card of the discard pile
   this.peek = function() {
     if (this.cards.length > 0) {
       return this.cards[this.cards.length - 1];
     } else {
-      return null;
+      return undefined;
+    }
+  }
+  // update the img element based on the top card of the pile.
+  this.render = function() {
+    var topCard = this.peek();
+    if (topCard != undefined) {
+      this.imgElement.setAttribute("src", "assets/deck/" + topCard.img);
+      this.imgElement.style.visibility = "visible";
+    } else {
+      this.imgElement.style.visibility = "hidden";
     }
   }
 }
 
-// Card object constructor
-function Card(index) {
+// XarnkerCard object constructor
+function XarnkerCard(index) {
   this.suit = suit(Math.floor((index-1) / 10));
   this.value = ((index-1) % 10) + 1;
   this.img = index + ".png";
 
+  // might come in handy for debugging?
   this.toString = function() {
     return this.value + " of " + this.suit;
   }
